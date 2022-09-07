ee58f0d01a01a62d20de487d40c52dbf60c03ca1
dudgus197493 <70511849+JeongHyun-kim-kor@users.noreply.github.com>
2022-09-07 16:13:27 +0900

    0907 5,6교시
    update, delete

--- ExployeeManagement/src/edu/kh/emp/model/dao/EmployeeDAO.java
+++ ExployeeManagement/src/edu/kh/emp/model/dao/EmployeeDAO.java
@@ -8,6 +8,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 import edu.kh.emp.model.vo.Employee;
 
@@ -375,6 +376,116 @@ public class EmployeeDAO {
 
 
 
+	/** 사번이 일치하는 사원 정보 수정 DAO
+	 * @param emp
+	 * @return result
+	 */
+	public int updateEmployee(Employee emp) {
+		int result = 0; // 결과 저장용 변수
+		
+		try {
+			Class.forName(driver);
+			conn = DriverManager.getConnection(url, user, pw);
+			conn.setAutoCommit(false); // AutoCommit 비활성화
+			
+			String sql = "UPDATE EMPLOYEE SET"
+					+ " EMAIL = ?, PHONE = ?, SALARY = ?"
+					+ " WHERE EMP_ID = ?";
+			
+			// PreparedStatement 생성
+			pstmt = conn.prepareStatement(sql);
+			
+			
+			//  ?에 알맞은 값을 세팅
+			pstmt.setString(1, emp.getEmail());
+			pstmt.setString(2, emp.getPhone());
+			pstmt.setInt(3, emp.getSalary());
+			pstmt.setInt(4, emp.getEmpId());
+			
+			result = pstmt.executeUpdate(); // 반영된 행의 개수 반환
+			
+			// 트랜잭션 제어처리
+			if(result == 0) conn.rollback();
+			else 			conn.commit();
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			
+			try {
+				// JDBC 객체 자원 반환
+				if(pstmt != null) pstmt.close();
+				if(conn != null) conn.close();
+				
+				
+				
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+			
+		}
+		
+		return result; // 결과 반환
+	}
+
+
+
+	public int deleteEmployee(int empId) {
+
+		int result = 0;
+			
+//		Employee emp = new Employee();
+		try {
+			Class.forName(driver);
+			conn = DriverManager.getConnection(url, user, pw);
+			conn.setAutoCommit(false);
+			
+			String sql = "DELETE FROM EMPLOYEE WHERE EMP_ID = ?";
+			
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setInt(1, empId);
+			
+			result = pstmt.executeUpdate();
+			
+			if(result >0) conn.commit();
+			else conn.rollback();
+			
+		} catch(Exception e	){
+			e.printStackTrace();
+		} finally {
+			
+			try {
+				if(pstmt != null) pstmt.close();
+				if(conn != null) conn.close();
+				
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		
+			
+		}
+		return result;
+	}
+
+
+
+	
+
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
